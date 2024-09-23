bf0c17f3f5efada679c972e410112fe7b31f1ac2
viisualworks <dshege4@gmail.com>
2024-09-23 14:43:59 +0300

    make the form responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 function App() {
 return (
 <main className="bg-dark-gray h-screen font-sans flex justify-center items-center">
-  <div className="py-5 pl-10 pr-4 w-3/5 h-min bg-white grid grid-cols-1 md:grid-cols-2 rounded-3xl place-content-center">
-    <div>
-      <h1 className="mt-20 text-4xl font-bold">Stay updated!</h1>
-      <p className="py-4">Join 60,000+ product managers receiving monthly updates on:</p>
-      <ul>
+  <div className="lg:py-5 lg:pl-10 lg:pr-4 lg:w-3/5 w-5/6 h-min bg-white grid grid-cols-1 lg:grid-cols-2 rounded-3xl place-content-center">
+    <div className="lg:order-1 order-2 lg:mt-20 my-10 mx-6">
+      <h1 className="md:text-4xl text-3xl font-bold">Stay updated!</h1>
+      <p className="md:text-base text-sm py-4">Join 60,000+ product managers receiving monthly updates on:</p>
+      <ul className="md:text-base text-sm">
         <li className="mb-2 flex items-center">
           <img src="/assets/icon-list.svg" alt="icon" className="mr-4 w-4 h-4" />
           Product discovery and building what matters
@@ -23,12 +23,14 @@ return (
       <form onSubmit="" className="mt-12 flex flex-col">
           <label className="mb-2 text-sm font-bold" htmlFor="email">Email address</label>
           <input className="py-3 px-8 rounded-md border-solid border-grey/75 border-2" type="text" id="email" name="email" placeholder="email@company.com" aria-required="true" />
-          <button className="bg-dark-gray text-white py-3 mt-4 rounded-lg hover:bg-gradient-to-r from-tomato to-orange-500" type="submit">Subscribe to monthly newsletter</button>
+          <button className="bg-dark-gray text-white py-3 mt-4 rounded-lg hover:bg-gradient-to-r from-tomato to-orange-500 transition duration-300" type="submit">Subscribe to monthly newsletter</button>
+
       </form>
 
     </div>
-    <div>
-      <img className="max-w-96 h-auto float-right" src="../public/assets/illustration-sign-up-desktop.svg" alt="" />
+    <div className="lg:order-2 order-1">
+      <img className="lg:block hidden max-w-96 h-auto float-right" src="../public/assets/illustration-sign-up-desktop.svg" alt="" />
+      <img className="lg:hidden block h-auto w-full" src="../public/assets/illustration-sign-up-mobile.svg" alt="" />
     </div>
   </div>
 </main>
